ca147bcf23fcb2043d349d4de72d96ef24e5a12c
Jonathan Piat <piat.jonathan@gmail.com>
2013-11-02 16:19:48 +0100

    mapping again ...

--- fbk_friend.py
+++ fbk_friend.py
@@ -5,7 +5,7 @@ flist_id = {100006530254783L: {'name' : u'Alice Santise', 'key' : 0x4e, 'index' 
 	562858291: {'name' : u'Joseph Durand', 'key' : 0x47, 'index' : 2, 'color' : 0x02}, 
 	516582206: {'name' : u'Marine Le Coz', 'key' : 0x1d, 'index' : 8, 'color' : 0x06}, 
 	1355840347: {'name' : u'Theo Durand', 'key' : 0x01e, 'index' : 9, 'color' : 0x02}, 
-	598964619: {'name' : u'Ga\xebl Le Coz', 'key' : 0x3e, 'index' : 10, 'color' : 0x04}, 
+	598964619: {'name' : u'Ga\xebl Le Coz', 'key' : 0x2e, 'index' : 10, 'color' : 0x04}, 
 	1110845350: {'name' : u'Anna Durand', 'key' : 0x27, 'index' : 5, 'color' : 0x06}, 
 	1011722861: {'name' : u'Jonathan Piat', 'key' : 0x4d, 'index' : 0, 'color' : 0x04},
 	715383253:  {'name' : u'F\xe9lix Smith', 'key' : 0x17, 'index' : 6, 'color' : 0x04} 
