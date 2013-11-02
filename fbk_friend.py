e3f188cc209a908c652f463177dc861c7e53a3fd
Jonathan Piat <piat.jonathan@gmail.com>
2013-11-02 16:16:50 +0100

    modifying key mapping

--- fbk_friend.py
+++ fbk_friend.py
@@ -1,12 +1,12 @@
-flist_id = {100006530254783L: {'name' : u'Alice Santise', 'key' : 0x0e, 'index' : 13, 'color' : 0x01}, 
-	710561363: {'name' : u'Poum-Poum Paris', 'key' : 0x0d, 'index' : 4, 'color' : 0x01}, 
-	563353866: {'name' : u'Gabrielle Durand', 'key' : 0x0b, 'index' : 12, 'color' : 0x06}, 
-	100001123034170L: {'name' : u'Armel Le Coz', 'key' : 0x07, 'index' : 3, 'color' : 0x04},
-	562858291: {'name' : u'Joseph Durand', 'key' : 0x1b, 'index' : 2, 'color' : 0x02}, 
+flist_id = {100006530254783L: {'name' : u'Alice Santise', 'key' : 0x4e, 'index' : 13, 'color' : 0x01}, 
+	710561363: {'name' : u'Poum-Poum Paris', 'key' : 0x07, 'index' : 4, 'color' : 0x01}, 
+	563353866: {'name' : u'Gabrielle Durand', 'key' : 0x3e, 'index' : 12, 'color' : 0x06}, 
+	100001123034170L: {'name' : u'Armel Le Coz', 'key' : 0x37, 'index' : 3, 'color' : 0x04},
+	562858291: {'name' : u'Joseph Durand', 'key' : 0x47, 'index' : 2, 'color' : 0x02}, 
 	516582206: {'name' : u'Marine Le Coz', 'key' : 0x1d, 'index' : 8, 'color' : 0x06}, 
 	1355840347: {'name' : u'Theo Durand', 'key' : 0x01e, 'index' : 9, 'color' : 0x02}, 
-	598964619: {'name' : u'Ga\xebl Le Coz', 'key' : 0x2b, 'index' : 10, 'color' : 0x04}, 
-	1110845350: {'name' : u'Anna Durand', 'key' : 0x17, 'index' : 5, 'color' : 0x06}, 
-	1011722861: {'name' : u'Jonathan Piat', 'key' : 0x2d, 'index' : 0, 'color' : 0x04},
-	715383253:  {'name' : u'F\xe9lix Smith', 'key' : 0x2e, 'index' : 6, 'color' : 0x04} 
+	598964619: {'name' : u'Ga\xebl Le Coz', 'key' : 0x3e, 'index' : 10, 'color' : 0x04}, 
+	1110845350: {'name' : u'Anna Durand', 'key' : 0x27, 'index' : 5, 'color' : 0x06}, 
+	1011722861: {'name' : u'Jonathan Piat', 'key' : 0x4d, 'index' : 0, 'color' : 0x04},
+	715383253:  {'name' : u'F\xe9lix Smith', 'key' : 0x17, 'index' : 6, 'color' : 0x04} 
 }
